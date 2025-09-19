8f84e28ab2807ee15ee907da5131c2ccd1b51a95
Naoise <s2848034@ed.ac.uk>
2025-09-19 18:59:58 +0100

    rename shakespeare text

---
+++ Group_Project_1/proj1.R
@@ -0,0 +1,8 @@
+#Naoise Daly s2848034
+
+setwd(r"(C:\Users\Naoise Daly\OneDrive - University of Edinburgh\stat prog\stats_programming_autumn2025\Group_Project_1)")
+
+#-------------Delete later
+a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
+          fileEncoding="UTF-8")
+head(a)
