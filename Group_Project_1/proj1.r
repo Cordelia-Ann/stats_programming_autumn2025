dc30aaece894e1d3091aa8d0c25e855271b35ede
HouseT5 <s2809867@ed.ac.uk>
2025-09-21 11:05:03 +0100

    current progress in section 4 of the project. I have done a number of trial methods to achieve the result. Currently working on looping through the set text to take all the words between brackets out.

--- Group_Project_1/proj1.r
+++ Group_Project_1/proj1.r
@@ -1,1 +1,18 @@
 
+setwd("C:\\Users\\toddh\\Stat Programming\\Statistical-Programming\\stats_programming_autumn2025\\stats_programming_autumn2025\\Group_Project_1\\stats_programming_autumn2025\\Group_Project_1") ## comment out of submitted
+a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83, fileEncoding="UTF-8")
+#test <- c("Be broken, [test my willingess] to [bring about] an end. Turn thy neighbor [james] into [nothing.")
+#test.split <- strsplit(test," ")[[1]] ##vector of shakespeare words
+#test.split
+open.bracket <- grep("[", a, fixed=TRUE)
+close.bracket <- grep("]", a, fixed=TRUE)
+data.length <- length(a)
+
+stage.directions <- c()
+index <- 1
+while(index <=length(open.bracket)) {
+  
+  
+  if ("]" %in% a[open.bracket:close.bracket]) {
+    close.index <- grepl("]", a, fixed=TRUE)}}
+
