cae8a0ba9aca4f645c3d1743cc462f1609b23853
HouseT5 <s2809867@ed.ac.uk>
2025-09-21 11:12:41 +0100

    added commentary to my code to try to explain what I am doing/trying to do. I am trying to switch back to my local branch.

--- Group_Project_1/proj1.r
+++ Group_Project_1/proj1.r
@@ -4,12 +4,13 @@ a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83, fileEncod
 #test <- c("Be broken, [test my willingess] to [bring about] an end. Turn thy neighbor [james] into [nothing.")
 #test.split <- strsplit(test," ")[[1]] ##vector of shakespeare words
 #test.split
-open.bracket <- grep("[", a, fixed=TRUE)
-close.bracket <- grep("]", a, fixed=TRUE)
-data.length <- length(a)
+open.bracket <- grep("[", a, fixed=TRUE) #this finds all the locations in a where there is an open bracket
+close.bracket <- grep("]", a, fixed=TRUE) #this finds all the locations in a where there is an close bracket
+data.length <- length(a) #this finds the total length of the shakespeare text
 
-stage.directions <- c()
-index <- 1
+stage.directions <- c() #creating an empty vector to put the stage directions into
+index <- 1 #this creates an index to reference below in the while loop to cycle through all values of open.bracket
+
 while(index <=length(open.bracket)) {
   
   
