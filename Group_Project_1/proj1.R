daeb5d428649f1c1376ff00f0f0f0532881b58aa
Naoise <s2848034@ed.ac.uk>
2025-09-22 16:19:39 +0100

    rename a variable to M1

--- Group_Project_1/proj1.R
+++ Group_Project_1/proj1.R
@@ -12,12 +12,12 @@ unique_words = unique(a);
 length(a);length(unique_words)
 # this replaces every word in the a, the text, with a number token
 # the word is replaced by the location/index of that word in unique_words
-a_tokenised = match(a, unique_words)
+M1 = match(a, unique_words)
 #----------change example later
 #for example, the first 24 words are unique but the 25th is a repetition of
 # "might" which already occurred as the 11th word 
 # so the 25th entry will have the token "11"
-cbind(a, a_tokenised)[11:25,]
+cbind(a, M1)[11:25,]
 
 #----------change to 1000 later
 #the model will use only the K most common words as part of its vocabulary
@@ -28,9 +28,9 @@ K_most_common_words = 20
 #and spits out the counts for each number, in order of that number
 #----------- change example later
 #i.e #times "1" appears, #times "2" appears, ...
-# the token "16" (representing "the") occurs 6 times in the a_tokenised
+# the token "16" (representing "the") occurs 6 times in the M1
 #so position 16 of the output is 6
-token_counts = tabulate(a_tokenised)
+token_counts = tabulate(M1)
 
 #order tells you what way to rearrange the locations so that 
 #the values are in decreasing order - biggest to smallest
@@ -43,7 +43,7 @@ b = unique_words[
 head(b);sum(a == b[1]) #'the' is the most common word
 
 #save space by deleting arrays we're done with
-rm(token_counts,a_tokenised, unique_words)
+rm(token_counts, unique_words)
 
 
 
