728b9021b8ba7500aec97b1afa7e385bc4e4fb3d
HouseT5 <s2809867@ed.ac.uk>
2025-09-23 09:44:20 +0100

    added names at top comment per instructions. Very brief description of work performed by each. We will want to give more detail at the end of the project when we know exactly what everyone did. I took Naoise's 'completed'? code from his branch and added it to the bottom of mine. I adjusted his input variables to match what the output of my section was. It runs without error and appears to function correctly. Need to discuss further.

--- Group_Project_1/proj1.r
+++ Group_Project_1/proj1.r
@@ -1,4 +1,5 @@
-
+#Naoise Daly s2848034, Todd House s2809867, Cordelia Bryant s2798199
+#Naoise built the functionality to cover #5-6. Todd built the data cleaning and filtering to cover #4. Cordelia built the functionality to implement 7-9.
 setwd("C:\\Users\\toddh\\Stat Programming\\Statistical-Programming\\stats_programming_autumn2025\\stats_programming_autumn2025\\Group_Project_1\\stats_programming_autumn2025\\Group_Project_1") ## comment out of submitted
 a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83, fileEncoding="UTF-8")
 
@@ -73,4 +74,96 @@ split_punct <- function(wordlist, punctuations) { #creating function to split pu
   return(unlist(output[1:(punct.counter-1)])) #unlists the output list into a vector over the length of the list minus one because the for loop added one extra to the counter
 }
 a.punct <- split_punct(a.no.underscore, punctuation.vec) #creates a new word list with the punctuations separated from the words using the split_punct function
-a.clean.lower <- tolower(a.punct) #this makes every word all lowercase
+a.clean.lower <- tolower(a.punct) #this makes every word
+
+#setwd(r"(C:\Users\Naoise Daly\OneDrive - University of Edinburgh\stat prog\stats_programming_autumn2025\Group_Project_1)")
+
+
+############ Q5 ############
+#------------- example data - Delete later
+#a <- tolower(scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
+#                  fileEncoding="UTF-8"))[4:108]
+
+unique_words = unique(a.clean.lower);
+length(a.clean.lower);length(unique_words)
+# this replaces every word in the a, the text, with a number token
+# the word is replaced by the location/index of that word in unique_words
+M1 = match(a.clean.lower, unique_words)
+#----------change example later
+#for example, the first 24 words are unique but the 25th is a repetition of
+# "might" which already occurred as the 11th word 
+# so the 25th entry will have the token "11"
+cbind(a.clean.lower, M1)[11:25,]
+
+#----------change to 1000 later
+#the model will use only the K most common words as part of its vocabulary
+K_most_common_words = 20
+
+#token/index counts
+#tabulate takes in the text tokenised as numbers 
+#and spits out the counts for each number, in order of that number
+#----------- change example later
+#i.e #times "1" appears, #times "2" appears, ...
+# the token "16" (representing "the") occurs 6 times in the M1
+#so position 16 of the output is 6
+token_counts = tabulate(M1)
+
+#order tells you what way to rearrange the locations so that 
+#the values are in decreasing order - biggest to smallest
+#in our case it orders the tokens by their count
+#so we can just take the first K of them 
+#the k most common tokens can then tell us the k most common words
+b = unique_words[
+  order(token_counts, decreasing = T)[1:K_most_common_words]
+]
+head(b);sum(a.clean.lower == b[1]) #'the' is the most common word
+
+#save space by deleting arrays we're done with
+rm(token_counts, unique_words)
+
+
+
+######### Q6 #############
+
+#so we are tokenising the text (for a second time) based on the vector b
+# each word in the text will be represented by a number token or NA
+#if the word is not in b, not one of the K most common words,
+# it is represented as NA
+#if the word is in b, it is represented by its location in b
+a_tokenised = match(a.clean.lower, b)
+
+
+#----------- example data - delete later
+# a_tokenised = strsplit(
+#   "A cat sat on the mat last night", " "
+# )[[1]]
+
+mlag = 20
+
+n = length(a_tokenised) #total number of words
+# I thought it was easier to manipulate M as an array first
+# remember that the length of each of column is n-mlag
+col_length = n-mlag
+M = array(NA, col_length*(mlag+1) )
+
+#M can be viewed as sliding a window of length col_length
+#over the vector a_tokenised
+#the first column of M is the first col_length elements of a_tokenised
+#the second column is the next col_length elements starting at the 2nd element
+#the ith column is the next col_length elements starting at the ith element
+
+#you don't like loops but this loop has only mlag iterations
+#and each iteration has constant time operations
+for (i in 0:mlag){
+  #the first col_length elements in M are column 1
+  #the elements in position col_length+1  to 2*col_length are column 2
+  column_i_indices = (i*(col_length) +1) : ((i+1)*col_length)
+  #now that we know where column i is in M,
+  #we insert the next tokens in the window
+  M[ column_i_indices  ]  = a_tokenised[i + 1:col_length ]
+}
+
+#now turn M from an array into a matrix
+# by default the matrix functions fills in column by column
+M <- matrix(M, n-mlag, mlag+1)
+dim(M)
