b85ff1bc681dba89368ccc3360166464fec6010f
Todd House <s2809867@ed.ac.uk>
2025-10-06 13:48:34 +0100

    Add files via upload

---
+++ Group_Project_2/proj2.R
@@ -0,0 +1,10 @@
+### Naoise Daly s2848034, Todd House s2809867, Cordelia Bryant s2798199 ###
+
+# Todd 
+# Naoise 
+# Cordelia 
+# We all collectively debugged and reviewed each others sections 
+
+
+
+#setwd("")
